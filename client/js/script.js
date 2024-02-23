cadbd5c0ecd97b67280ae362e1b5cc612434a8ae
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-02-23 14:19:04 -0300

    feat: sign up & home screen

--- client/js/script.js
+++ client/js/script.js
@@ -1,12 +1,13 @@
 const title = $("title").text();
+const localName = localStorage.getItem("userName");
+const localEmail = localStorage.getItem("userEmail");
 
-if (title.toLowerCase() === "login") {
+if (title.toLowerCase() === "login" || title.toLowerCase() === "signup") {
   console.log("Route /");
 } else {
-  const localName = localStorage.getItem("userName");
-  const localEmail = localStorage.getItem("userEmail");
-
-  if (!localName && !localEmail) window.location.href = "/";
+  if (!localName && !localEmail) {
+    window.location.href = "/";
+  } 
 }
 
 function loginOnSubmit() {
@@ -18,7 +19,6 @@ function loginOnSubmit() {
     PW_USER: password.toString(),
   };
 
-  
   $.ajax({
     url: "http://localhost:5000/api/account/login",
     type: "POST",
@@ -37,4 +37,63 @@ function loginOnSubmit() {
       alert(err);
     },
   });
+}
+
+$("#menu-icon").on("click", () => {
+  $("#navbar").toggleClass("active");
+});
+
+
+$("#welcome-again-local").text(`Welcome, ${localName}`);
+
+function logOut() {
+  localStorage.clear();
+  window.location.replace("/");
+  return "Saiu"
+}
+
+function createOnSubmit() {
+  const pw1 = $("#PW_USER").val().toString();
+  const pw2 = $("#PW_USER_CONF").val().toString();
+
+  if (pw2 === pw1) {
+    const name = $("#NM_USER").val();
+    const email = $("#EM_USER").val().toLowerCase();
+    const password = $("#PW_USER").val();
+    const cellphone = $("#TF_USER").val();
+  
+    const postData = {
+      NM_USER: name.toString(),
+      EM_USER: email.toString(),
+      PW_USER: password.toString(),
+      TF_USER: cellphone.toString(),
+    };
+
+    try {
+      $.ajax({
+        url: "http://localhost:5000/api/account/create",
+        type: "POST",
+        data: JSON.stringify(postData),
+        contentType: "application/json",
+        success: function (res) {
+          if (res != "Erro ao inserir") {
+            console.log("funcionou");
+            alert("Usuário cadastrado");
+            
+
+            window.location.href = "/";
+          } else {
+            alert("Erro interno");
+          }
+        },
+        error: function (err) {
+          alert(err);
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    alert("Passwords does not match.");
+  }
 }
